92e666cf19afa01c62f764e3331ae3a40760ec57
Noel Worden <noelworden@gmail.com>
2016-09-10 15:56:40 -0600

    [FEAT] parks controller [CHG] locations show page to display results
    The locations/show page now shows the top 3 parks based on ip address
    from user. The link takes user to park:id show page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,7 +5,7 @@ def index
 
   # @parks = Park.all
 
-  @locations = Park.near("Erie, CO", 5000)
+  @locations = Park.near("Erie, CO", 5000) ## this shows on the index the parks near erie
 
   ## after submit go to show page with the method being 
   ## @ location = Park.near(@location.lat, @location.long, 1000)
@@ -14,7 +14,8 @@ end
 def show
   @location = Location.find(params[:id])
 
-  # @parks = Location.near("Boulder, CO", 40)
+  @parks = Park.near([@location.latitude, @location.longitude], 500)
+
 end
 
 def new
--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,5 +1,8 @@
 class ParksController < ApplicationController
-
+  def index
+    @parks = Park.all
+  end
+  
   def show
     @park = Park.find(params[:id])
   end
