f7f2e0745c7d6bc4ed6e836435b66a8d44da4f8d
Noel Worden <noelworden@gmail.com>
2016-09-10 09:18:31 -0600

    [CHG] cleaned up routes.rb

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,10 @@
 class Location < ActiveRecord::Base
-  # geocoded_by :ip       #this is the field needed by geocode gem to know what is to be converted to lat/long
-  # after_validation :geocode, :if => :ip_changed?
+  #TODO clean
+  geocoded_by :ip       #this is the field needed by geocode gem to know what is to be converted to lat/long
+  after_validation :geocode, :if => :ip_changed?
 
   #second scope should now take ip OR address and change it to lat/long
-  geocoded_by :address
-  after_validation :geocode, :if => :address_changed?
+  # geocoded_by :address
+  # after_validation :geocode, :if => :address_changed?
 
 end
