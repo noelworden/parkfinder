d72c13953bc27fb3754e410fce5f25cac7697247
Noel Worden <noelworden@gmail.com>
2016-09-11 22:25:37 -0600

    [CHG] seed.rb to populate Parks table [CHG] minor changes to views & controller
    Making the site more presentable. Made a full database of the natl
    parks

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,4 +31,335 @@ Park.create(
   address: "25 Visitor Center Rd, Bar Harbor, ME 04609",
   )
 
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Glacier National Park",
+  address: "Glacier National Park, Flathead County, MT",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Arches National Park",
+  address: "2180 N Hwy 191, Moab, UT 84532",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Badlands National Park",
+  address: "Arches Entrance Rd Moab, UT 84532",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Big Bend National Park",
+  address: "Main Park Rd, Alpine, TX 79830",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "",
+  address: "",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Main Park Rd, Alpine, TX 79830",
+  address: "Bryce Canyon National Park, Garfield County, Utah 84764",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Canyonlands National Park",
+  address: "Canyonlands National Park, San Juan County, UT",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Capitol Reef National Park",
+  address: "Capitol Reef National Park Visitor Center, Scenic Drive, Torrey, UT",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Carlsbad Caverns National Park",
+  address: "Carlsbad Caverns National Park, Eddy County, NM",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Channel Islands National Park",
+  address: "Channel Islands National Park, Ventura, CA",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Congaree National Park",
+  address: "Congaree National Park, National Park Road, Hopkins, SC",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Cuyahoga Valley National Park",
+  address: "Cuyahoga Valley National Park, Vaughn Road, Brecksville, OH",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "",
+  address: "",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Death Valley National Park",
+  address: "Death Valley National Park",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Denali National Park & Preserve",
+  address: "Denali National Park & Preserve Headquarters, Denali National Park and Preserve, AK",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Dry Tortugas National Park",
+  address: "Dry Tortugas National Park, Monroe County, FL",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Everglades National Park",
+  address: "40001 State Highway 9336, Homestead, FL 33034",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Gates of the Arctic National Park",
+  address: "Gates of the Arctic National Park and Preserve, Bettles, AK",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Glacier Bay National Park",
+  address: "Glacier Bay National Park and Preserve, Hoonah-Angoon, AK",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Glacer National Park",
+  address: "Glacier National Park, Flathead County, MT",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Grand Canyon National Park",
+  address: "Grand Canyon National Park Headquarters, South Entrance Road, Grand Canyon Village, Coconino County, AZ",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Grand Teton National Park",
+  address: "Grand Teton National Park Entrance Turnout, Jackson, WY",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Great Basin National Park",
+  address: "5500 West Highway 488, Baker, NV 89311",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Great Sand Dunes National Park & Preserve",
+  address: "Great Sand Dunes National Park and Preserve, 11999 Colorado 150, Mosca, CO 81146",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Great Smokey Mountains National Park",
+  address: "107 Park Headquarters Rd, Gatlinburg, TN 37738",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Guadalupe Mountains National Park",
+  address: "400 Pine Canyon Dr, Salt Flat, TX 79847",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Haleakala National Park",
+  address: "Haleakala National Park",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Hawaii Volcanoes National Park",
+  address: "1 Crater Rim Drive, Volcano, HI 96785",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Hot Springs National Park",
+  address: "369 Central Ave, Hot Springs, AR 71901",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Isle Royale National Park",
+  address: "Isle Royale National Park",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Joshua Tree National Park",
+  address: "Joshua Tree National Park, Pinto Basin Rd, California 92277",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Katmai National Park & Preserve",
+  address: "Lake Camp Road, King Salmon, AK 99613",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Kenai Fjords National Park",
+  address: "1212 4th Ave, Seward, AK 99664",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Kings Canyon National Park",
+  address: "83918 California 180, Hume, CA 93628",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Kobuk Valley National Park",
+  address: "Kobuk Valley National Park",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Lake Clark National Park",
+  address: "Lake Clark National Park Visitor Center, Port Alsworth, AK",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Lassen Volcanic National Park",
+  address: "McCullough Road Port Alsworth, AK 99653",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Mammoth Cave National Park",
+  address: "1 Mammoth Cave Pkwy, Mammoth Cave, KY 42259",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Mount Rainier National Park",
+  address: "Mount Rainier National Park, Sunrise Park Rd, Ashford, WA 98304",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "National Park of American Samoa",
+  address: "National Park of American Samoa",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "North Cascades National Park",
+  address: "Marblemount, WA 98267",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Olympic National Park",
+  address: "3002 Mt Angeles Rd, Port Angeles, WA 98362",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Petrified Forest National Park",
+  address: "1 Park Rd, Petrified Forest, AZ 86028",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Redwood National Park",
+  address: "119441 U.S. 101, Orick, CA 95555",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Saquaro National Park",
+  address: "2700 N Kinney Rd, Tucson, AZ 85743",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Sequoia National Park",
+  address: "63100 Lodgepole Rd, Sequoia National Park, CA 93262",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Shanandoah National Park",
+  address: "Shenandoah National Park, Skyline Drive Mile 4.6, Front Royal, VA 22630",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Theodore Roosevelt National Park",
+  address: "Exit 32, Belfield, ND 58622",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Virgin Islands National Park",
+  address: "Virgin Island National Park, St John 00830, U.S. Virgin Islands",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Voyagers National Park",
+  address: "1797 Town Rd 342, International Falls, MN 56649",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Wind Cave National Park",
+  address: "6611 US Highway 385, Hot Springs, SD 57747",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Wrangell - St. Elias National Park & Preserve",
+  address: "Wrangell-Saint-Elias Visitor Center, Richardson Highway, Copper Center, AK",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Yellowstone National Park",
+  address: "30 Yellowstone Ave, West Yellowstone, MT 59758",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Yosemite National Park",
+  address: "Yosemite National Park, 9035 Village Dr, YOSEMITE NATIONAL PARK, CA 95389",
+    )
+
+Park.create(
+  parktype: Park::PARK_TYPE[0],
+  parkname: "Zion National Park",
+  address: "1 Zion Park Blvd, Springdale, UT 84767",
+    )
+
+
 puts "***#{Park.count} park database items created!***"
